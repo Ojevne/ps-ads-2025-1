15a3003359cb0a9b23b1617e162cffad07985394
Ojevne <lexluthor55@hotmail.com>
2025-06-18 04:13:43 -0300

    corrige múltiplas instâncias do Prisma (prepared statement)

--- back-end/src/database/client.js
+++ back-end/src/database/client.js
@@ -1,18 +1,28 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient({
-  log: [
-    {
-      emit: 'event',
-      level: 'query'
-    }
-  ]
-})
+const globalForPrisma = globalThis
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: [
+      {
+        emit: 'event',
+        level: 'query'
+      }
+    ]
+  })
 
 // Exibe no console as instruções SQL enviadas ao BD
 prisma.$on('query', event => {
   console.log('-'.repeat(60))
   console.log(event.query)
-  if(event.params) console.log('PARAMS:', event.params)
+  if (event.params) console.log('PARAMS:', event.params)
 })
+
+// Previne múltiplas instâncias no ambiente dev/hot reload
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
+
 export default prisma
